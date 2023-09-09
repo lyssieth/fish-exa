4229e34826641ab28760ee030c3aa3adf53737b3
Lys <lys@lys.ee>
2023-09-09 03:27:47 +0300

    whoops
    I forgot to change the naaame~

--- functions/lg.fish
+++ functions/lg.fish
@@ -1,3 +1,3 @@
-function la -d 'list files (with git repo status) using eza' -w eza
+function lg -d 'list files (with git repo status) using eza' -w eza
     command eza -l --group-directories-first --git --git-repos $argv
 end
