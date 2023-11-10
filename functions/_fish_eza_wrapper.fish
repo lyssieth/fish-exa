6d2173fdcf24dea804fb50242a5cb98210147a24
Lyssieth <lys@lys.ee>
2023-11-10 17:55:20 +0200

    feat: color-scale only size

--- functions/_fish_eza_wrapper.fish
+++ functions/_fish_eza_wrapper.fish
@@ -1,3 +1,3 @@
 function _fish_eza_wrapper -w eza
-    command eza --git --smart-group --group-directories-first --icons=auto --color=auto --color-scale all $argv
+    command eza --git --smart-group --group-directories-first --icons=auto --color=auto --color-scale size $argv
 end
